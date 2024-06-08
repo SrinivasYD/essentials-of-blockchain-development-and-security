d46c3e6023691f6cdd408390ecb3fab6b7b13918
SrinivasYD <imyds1612@gmail.com>
2024-06-08 06:06:30 -0400

    Finalize PoW implementation, add tests and comprehensive README

--- 1-class/examples/pow-blockchain/Blockchain.js
+++ 1-class/examples/pow-blockchain/Blockchain.js
@@ -1,147 +1,238 @@
-const crypto = require('crypto');
-const uuid = require('uuid');
+const crypto = require("crypto");
+const { v4: uuidv4 } = require("uuid");
 
 /**
- * Block represents a block in the blockchain. It has the
- * following params:
- * @index represents its position in the blockchain
- * @timestamp shows when it was created
- * @transactions represents the data about transactions
- * added to the chain
- * @hash represents the hash of the previous block
+ * Represents a block in the blockchain.
+ * @class Block
  */
 class Block {
-    constructor(index, transactions, prevHash, nonce, hash) {
-        this.index = index;
-        this.timestamp = Math.floor(Date.now() / 1000);
-        this.transactions = transactions;
-        this.prevHash = prevHash;
-        this.hash = hash;
-        this.nonce = nonce;
-    }
+  /**
+   * Creates an instance of Block.
+   * @param {number} index - Position of the block in the blockchain.
+   * @param {Array} transactions - List of transactions in the block.
+   * @param {string} prevHash - Hash of the previous block.
+   * @param {number} nonce - Nonce value for proof of work.
+   * @param {string} hash - Hash of the current block.
+   */
+  constructor(index, transactions, prevHash, nonce, hash) {
+    this.index = index;
+    this.timestamp = Math.floor(Date.now() / 1000);
+    this.transactions = transactions;
+    this.prevHash = prevHash;
+    this.hash = hash;
+    this.nonce = nonce;
+  }
 }
 
 /**
- * A blockchain transaction. Has an amount, sender and a
- * recipient (not UTXO).
+ * Represents a transaction in the blockchain.
+ * @class Transaction
  */
 class Transaction {
-    constructor(amount, sender, recipient) {
-        this.amount = amount;
-        this.sender = sender;
-        this.recipient = recipient;
-        this.tx_id = uuid().split('-').join();
-    }
+  /**
+   * Creates an instance of Transaction.
+   * @param {number} amount - Amount of the transaction.
+   * @param {string} sender - ID of the sender.
+   * @param {string} recipient - ID of the recipient.
+   */
+  constructor(amount, sender, recipient) {
+    this.amount = amount;
+    this.sender = sender;
+    this.recipient = recipient;
+    this.tx_id = uuidv4().split("-").join();
+  }
 }
 
 /**
- * Blockchain represents the entire blockchain with the
- * ability to create transactions, mine and validate
- * all blocks.
+ * Represents the entire blockchain.
+ * @class Blockchain
  */
 class Blockchain {
-    constructor() {
-        this.chain = [];
-        this.pendingTransactions = [];
-        this.addBlock('0');
+  /**
+   * Creates an instance of Blockchain.
+   */
+  constructor() {
+    this.chain = [];
+    this.pendingTransactions = [];
+    this.difficulty = 3; // Initial difficulty level
+    this.addBlock("0");
+  }
+
+  /**
+   * Creates a new transaction and adds it to the list of pending transactions.
+   * @param {number} amount - Amount of the transaction.
+   * @param {string} sender - ID of the sender.
+   * @param {string} recipient - ID of the recipient.
+   */
+  createTransaction(amount, sender, recipient) {
+    this.pendingTransactions.push(new Transaction(amount, sender, recipient));
+  }
+
+  /**
+   * Adds a block to the blockchain.
+   * @param {number} nonce - Nonce value found by the proof of work algorithm.
+   */
+  addBlock(nonce) {
+    const index = this.chain.length;
+    const prevHash =
+      this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : "0";
+    const hash = this.getHash(prevHash, this.pendingTransactions, nonce);
+    const block = new Block(
+      index,
+      this.pendingTransactions,
+      prevHash,
+      nonce,
+      hash
+    );
+
+    // Reset pending transactions
+    this.pendingTransactions = [];
+    this.chain.push(block);
+
+    // Adjust difficulty if necessary
+    this.adjustDifficulty();
+  }
+
+  /**
+   * Adjusts the difficulty of the proof of work algorithm.
+   */
+  adjustDifficulty() {
+    if (this.chain.length % 10 === 0) {
+      // Increase difficulty every 10 blocks
+      this.difficulty++;
+    }
+  }
+
+  /**
+   * Computes the hash of a block.
+   * @param {string} prevHash - Hash of the previous block.
+   * @param {Array} txs - List of transactions in the block.
+   * @param {number} nonce - Nonce value.
+   * @returns {string} - Computed hash of the block.
+   */
+  getHash(prevHash, txs, nonce) {
+    let encrypt = prevHash + nonce;
+    txs.forEach((tx) => {
+      encrypt += tx.tx_id;
+    });
+    const hash = crypto
+      .createHmac("sha256", "secret")
+      .update(encrypt)
+      .digest("hex");
+    return hash;
+  }
+
+  /**
+   * Finds a nonce that satisfies the proof of work requirement.
+   * @param {string} prevHash - Hash of the previous block.
+   * @param {Array} transactions - List of transactions in the block.
+   * @param {number} difficulty - Difficulty level.
+   * @returns {number} - Nonce value that satisfies the proof of work.
+   */
+  proofOfWork(prevHash, transactions, difficulty) {
+    let nonce = 0;
+    let hash = this.getHash(prevHash, transactions, nonce);
+    const target = "0".repeat(difficulty); // Target hash must have leading zeros
+
+    while (!hash.startsWith(target)) {
+      nonce++;
+      hash = this.getHash(prevHash, transactions, nonce);
     }
 
-    /**
-     * Creates a transaction on the blockchain
-     */
-    createTransaction(amount, sender, recipient) {
-        this.pendingTransactions.push(new Transaction(amount, sender, recipient));
+    return nonce;
+  }
+
+  /**
+   * Mines a new block and adds it to the chain.
+   */
+  mine() {
+    const startTime = Date.now();
+    const prevHash =
+      this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : "0";
+    const nonce = this.proofOfWork(
+      prevHash,
+      this.pendingTransactions,
+      this.difficulty
+    );
+    this.addBlock(nonce);
+    const endTime = Date.now();
+
+    const timeTaken = (endTime - startTime) / 1000; // Time taken in seconds
+    console.log(`Block mined in ${timeTaken} seconds`);
+
+    // Compute the average hash rate (hashes per second)
+    const totalHashes = nonce + 1; // Since nonce starts at 0
+    const hashRate = totalHashes / timeTaken;
+    console.log(`Average hash rate: ${hashRate} hashes per second`);
+  }
+
+  /**
+   * Validates the blockchain by ensuring the integrity of all blocks.
+   * @returns {boolean} - True if the blockchain is valid, false otherwise.
+   */
+  chainIsValid() {
+    for (let i = 0; i < this.chain.length; i++) {
+      const tx_id_list = [];
+      this.chain[i].transactions.forEach((tx) => tx_id_list.push(tx.tx_id));
+
+      if (i === 0 && this.chain[i].hash !== this.getHash("0", [], "0")) {
+        return false;
+      }
+      if (
+        i > 0 &&
+        this.chain[i].hash !==
+          this.getHash(
+            this.chain[i - 1].hash,
+            this.chain[i].transactions,
+            this.chain[i].nonce
+          )
+      ) {
+        return false;
+      }
+      if (i > 0 && this.chain[i].prevHash !== this.chain[i - 1].hash) {
+        return false;
+      }
     }
-
-    /**
-     * Add a block to the blockchain
-     */
-    addBlock(nonce) {
-        let index = this.chain.length;
-        let prevHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : '0';
-        let hash = this.getHash(prevHash, this.pendingTransactions, nonce);
-        let block = new Block(index, this.pendingTransactions, prevHash, nonce, hash);
-
-        // reset pending txs
-        this.pendingTransactions = [];
-        this.chain.push(block);
-    }
-
-    /**
-     * Gets the hash of a block.
-     */
-    getHash(prevHash, txs, nonce) {
-        var encrypt = prevHash + nonce;
-        txs.forEach((tx) => { encrypt += tx.tx_id; });
-        var hash=crypto.createHmac('sha256', "secret")
-            .update(encrypt)
-            .digest('hex');
-        return hash;
-    }
-
-    /**
-     * Find nonce that satisfies our proof of work.
-     */
-    proofOfWork() {
-        //TODO
-    }
-
-    /**
-     * Mine a block and add it to the chain.
-     */
-    mine() {
-        let tx_id_list = [];
-        this.pendingTransactions.forEach((tx) => tx_id_list.push(tx.tx_id));
-        let nonce = this.proofOfWork();
-        this.addBlock('0');
-    }
-
-
-    /**
-     * Check if the chain is valid by going through all blocks and comparing their stored
-     * hash with the computed hash.
-     */
-    chainIsValid(){
-        for(var i=0;i<this.chain.length;i++){
-            let tx_id_list = [];
-            this.chain[i].transactions.forEach((tx) => tx_id_list.push(tx.tx_id));
-
-            if(i == 0 && this.chain[i].hash !==this.getHash('0',[],'0')){
-                return false;
-            }
-            if(i > 0 && this.chain[i].hash !== this.getHash(this.chain[i-1].hash, this.chain[i].transactions, '0')){
-                return false;
-            }
-            if(i > 0 && this.chain[i].prevHash !== this.chain[i-1].hash){
-                return false;
-            }
-        }
-        return true;
-    }
+    return true;
+  }
 }
 
+/**
+ * Constructs a Merkle tree (placeholder function).
+ * @param {Array} inputs - Array of inputs for the Merkle tree.
+ */
 function constructMerkleTree(inputs) {
-    //TODO
+  // TODO: Implement Merkle tree construction
 }
 
+/**
+ * Simulates the blockchain by creating and mining transactions.
+ * @param {Blockchain} blockchain - The blockchain instance.
+ * @param {number} numTxs - Number of transactions per block.
+ * @param {number} numBlocks - Number of blocks to mine.
+ */
 function simulateChain(blockchain, numTxs, numBlocks) {
-    for(let i = 0; i < numBlocks; i++) {
-        let numTxsRand = Math.floor(Math.random() * Math.floor(numTxs));
-        for(let j = 0; j < numTxsRand; j++) {
-            let sender = uuid().substr(0,5);
-            let receiver = uuid().substr(0,5);
-            blockchain.createTransaction(sender, receiver,
-                                         Math.floor(Math.random() * Math.floor(1000)));
-        }
-        blockchain.mine();
+  for (let i = 0; i < numBlocks; i++) {
+    const numTxsRand = Math.floor(Math.random() * Math.floor(numTxs));
+    for (let j = 0; j < numTxsRand; j++) {
+      const sender = uuidv4().substr(0, 5);
+      const receiver = uuidv4().substr(0, 5);
+      blockchain.createTransaction(
+        Math.floor(Math.random() * 1000),
+        sender,
+        receiver
+      );
     }
+    blockchain.mine();
+  }
 }
 
+// Create a new blockchain instance and simulate transactions
 const BChain = new Blockchain();
 simulateChain(BChain, 5, 3);
 
 module.exports = Blockchain;
 
-// uncomment these to run a simulation
-// console.dir(BChain,{depth:null});
+// Uncomment these lines to run a simulation and check the blockchain's validity
+// console.dir(BChain, { depth: null });
 // console.log("******** Validity of this blockchain: ", BChain.chainIsValid());
--- 1-class/examples/pow-blockchain/README.md
+++ 1-class/examples/pow-blockchain/README.md
@@ -3,18 +3,314 @@
 This is a blockchain simulator where transactions can be added to a mempool and can be mined into a block using PoW.
 
 ## Running
+
 1. Install required modules: `npm install #might need sudo`
 2. Run a node on one terminal by specifying a port: `PORT=8000 && node blockchain-peer.js`
 3. Run as many nodes as you want in separate terminals using the above command with different ports
 
 ## Interacting with the blockchain
+
 You can simulate transactions using some HTTP request client like cURL (terminal) or [Postman](https://www.postman.com/) (GUI).
+
 - You can start by making nodes find each other using this request body `{ "peer": "http://localhost:8081" }` and sending it to one of the nodes as a POST request to the endpoint `http://localhost:8080/add_known_peer`
 - Then, you can make nodes broadcast by passing this request body `{ "msg": "Hello world!"}` as a POST request to the endpoint `http://localhost:8080/broadcast`
 - You can then interact with the blockchain API methods as documented in the code
 
 ## Docker
+
 You can run a node as a docker container.
+
 - First, build the container using: `docker build .`. This will generate a random container ID.
 - Then, you can run the node by specifying a port (such as 8080): `export PORT=8000 && docker run -e PORT=$PORT --expose $PORT -p $PORT:$PORT <CONTAINER-ID>`
 - This will spin up a node you on your port that you can make requests to
+
+---
+
+# Proof of Work Blockchain Assignment
+
+## Name: Durga Srinivas Yerasuri
+
+## Student Number: 220936027
+
+## Assignment: 1
+
+### Project Overview
+
+This project implements a basic proof of work (PoW) blockchain in Node.js. The blockchain supports transaction creation, block mining, and validation. The difficulty of mining increases after every 10 blocks to simulate a more challenging mining environment.
+
+### Approach
+
+1. **Class Design:**
+
+   - `Block` class to represent individual blocks.
+   - `Transaction` class to represent transactions.
+   - `Blockchain` class to manage the chain of blocks, transaction pool, and mining process.
+
+2. **Proof of Work Algorithm:**
+
+   - Implemented in the `proofOfWork` method of the `Blockchain` class.
+   - The method calculates the nonce such that the hash of the block starts with a specified number of leading zeros based on the difficulty level.
+
+3. **Difficulty Adjustment:**
+
+   - The difficulty increases after every 10 blocks, making the mining process more challenging.
+
+4. **Hash Rate Calculation:**
+   - During the mining process, the time taken to find a valid nonce is recorded, and the hash rate is calculated and logged.
+
+### How to Test the Functionality
+
+1. **Install Dependencies:**
+
+   - Ensure Node.js is installed.
+   - Run `npm install` to install the required dependencies listed in `package.json`.
+
+2. **Run the Test Script:**
+   - Execute `node testBlockchain.js` to run the test script.
+   - The script will create transactions, mine blocks, and validate the blockchain.
+   - Check the console output to verify the following:
+     - Hashes of the blocks meet the difficulty requirement.
+     - Difficulty increases after 10 blocks.
+     - Hash rate is calculated and logged.
+     - The blockchain is valid.
+
+### Project Files
+
+- `Blockchain.js`: Contains the implementation of the blockchain, block, and transaction classes.
+- `blockchain-peer.js`: Provides an Express server to interact with the blockchain.
+- `Dockerfile`: Configuration for Docker to run the blockchain application.
+- `package.json`: Lists project dependencies.
+- `testBlockchain.js`: Script to test the blockchain functionality.
+- `README.md`: Documentation file.
+
+### Example Output after executing 'node testBlockchain.js' command
+
+Block mined in 0.115 seconds
+Average hash rate: 94895.65217391304 hashes per second
+Block mined in 0.031 seconds
+Average hash rate: 95064.51612903226 hashes per second
+Block mined in 0.025 seconds
+Average hash rate: 81200 hashes per second
+Creating transactions...
+Mining block...
+Block mined in 0.149 seconds
+Average hash rate: 94315.43624161075 hashes per second
+First block mined with hash: 0005cfaedf2fa8ac6d529c7e613d51b1ded3f8bfcb844c89ddbeff5c79f328c0 (difficulty: 3)
+Mining additional blocks to test difficulty adjustment...
+Block mined in 0.025 seconds
+Average hash rate: 92120 hashes per second
+Block mined in 0.037 seconds
+Average hash rate: 126378.37837837839 hashes per second
+Block mined in 0.028 seconds
+Average hash rate: 125714.28571428571 hashes per second
+Block mined in 0.027 seconds
+Average hash rate: 129444.44444444445 hashes per second
+Block mined in 0.011 seconds
+Average hash rate: 97454.54545454546 hashes per second
+Block mined in 0.017 seconds
+Average hash rate: 125294.11764705881 hashes per second
+Block mined in 0.009 seconds
+Average hash rate: 85888.88888888889 hashes per second
+Block mined in 0.055 seconds
+Average hash rate: 126054.54545454546 hashes per second
+Block mined in 0.896 seconds
+Average hash rate: 144213.16964285713 hashes per second
+Checking difficulty adjustment...
+Current difficulty: 4
+Checking hash rate calculation...
+Block mined in 0.34 seconds
+Average hash rate: 138355.88235294117 hashes per second
+Final blockchain:
+Blockchain {
+chain: [
+Block {
+index: 0,
+timestamp: 1717840207,
+transactions: [],
+prevHash: '0',
+hash: '3a17d38f56ca57a0af46641df698103964e32832fafb27419a19232fe42e64a6',
+nonce: '0'
+},
+Block {
+index: 1,
+timestamp: 1717840207,
+transactions: [
+Transaction {
+amount: 10,
+sender: 'Alice',
+recipient: 'Bob',
+tx_id: '8d2e3f24,fbf2,4106,aa47,54c66d16dbac'
+},
+Transaction {
+amount: 20,
+sender: 'Bob',
+recipient: 'Charlie',
+tx_id: '65517ed9,9fba,4203,b51c,0bc93fa991a9'
+},
+Transaction {
+amount: 30,
+sender: 'Charlie',
+recipient: 'Dave',
+tx_id: '5892a371,5876,42a1,8a27,5f755097f6f6'
+}
+],
+prevHash: '3a17d38f56ca57a0af46641df698103964e32832fafb27419a19232fe42e64a6',
+hash: '0005cfaedf2fa8ac6d529c7e613d51b1ded3f8bfcb844c89ddbeff5c79f328c0',
+nonce: 14052
+},
+Block {
+index: 2,
+timestamp: 1717840207,
+transactions: [
+Transaction {
+amount: 69.33215385991427,
+sender: 'UserA',
+recipient: 'UserB',
+tx_id: 'a3602d37,000e,4287,a5f6,269f67188940'
+}
+],
+prevHash: '0005cfaedf2fa8ac6d529c7e613d51b1ded3f8bfcb844c89ddbeff5c79f328c0',
+hash: '000cc87b47b44d9f6466c2aae5015c0625587b8d116afbfbf22c48445ffe0a43',
+nonce: 2302
+},
+Block {
+index: 3,
+timestamp: 1717840207,
+transactions: [
+Transaction {
+amount: 97.78988926664742,
+sender: 'UserA',
+recipient: 'UserB',
+tx_id: 'fa2cafed,695c,45fe,a15a,760d3c59aef6'
+}
+],
+prevHash: '000cc87b47b44d9f6466c2aae5015c0625587b8d116afbfbf22c48445ffe0a43',
+hash: '000fd3dee6aaec8b0edf2a22dd815924824fc81e2dce8598ded441a711d10fb3',
+nonce: 4675
+},
+Block {
+index: 4,
+timestamp: 1717840207,
+transactions: [
+Transaction {
+amount: 84.34898391220096,
+sender: 'UserA',
+recipient: 'UserB',
+tx_id: '0b9151a1,d589,41a2,bc5f,dcdb5d85f93c'
+}
+],
+prevHash: '000fd3dee6aaec8b0edf2a22dd815924824fc81e2dce8598ded441a711d10fb3',
+hash: '000a9374a77644a619c4945bf4feecfdbc1be25d1b2a8e82a117ca00a212456c',
+nonce: 3519
+},
+Block {
+index: 5,
+timestamp: 1717840207,
+transactions: [
+Transaction {
+amount: 43.16201525650287,
+sender: 'UserA',
+recipient: 'UserB',
+tx_id: '5cd091ed,c461,4d04,9960,51de91c2953d'
+}
+],
+prevHash: '000a9374a77644a619c4945bf4feecfdbc1be25d1b2a8e82a117ca00a212456c',
+hash: '000fb8a7d68ef68a5df38fe5037275c34628434b0e78a29576532fbc74e3a63d',
+nonce: 3494
+},
+Block {
+index: 6,
+timestamp: 1717840207,
+transactions: [
+Transaction {
+amount: 29.76508092302801,
+sender: 'UserA',
+recipient: 'UserB',
+tx_id: '9308886d,66a4,40e2,aa5a,785ea3565236'
+}
+],
+prevHash: '000fb8a7d68ef68a5df38fe5037275c34628434b0e78a29576532fbc74e3a63d',
+hash: '000006878ee6b974c6ad11078e377a2c6eac9ea8fede796287c717aa16d1396b',
+nonce: 1071
+},
+Block {
+index: 7,
+timestamp: 1717840207,
+transactions: [
+Transaction {
+amount: 87.34576692050213,
+sender: 'UserA',
+recipient: 'UserB',
+tx_id: '105a497f,b7c2,4388,86f0,59301c58a552'
+}
+],
+prevHash: '000006878ee6b974c6ad11078e377a2c6eac9ea8fede796287c717aa16d1396b',
+hash: '000b0bedd9061b3ed294b4942afb459eaf00a1f897b6f17d3de6889fbefef8be',
+nonce: 2129
+},
+Block {
+index: 8,
+timestamp: 1717840207,
+transactions: [
+Transaction {
+amount: 13.019188632498357,
+sender: 'UserA',
+recipient: 'UserB',
+tx_id: 'ced415ec,bca4,4f30,886f,171b5a2284ba'
+}
+],
+prevHash: '000b0bedd9061b3ed294b4942afb459eaf00a1f897b6f17d3de6889fbefef8be',
+hash: '000235d64a8a519ae6cc5345061ec1ca87d3f2a5dcfa3c271e6a220f22437286',
+nonce: 772
+},
+Block {
+index: 9,
+timestamp: 1717840207,
+transactions: [
+Transaction {
+amount: 65.32793463912103,
+sender: 'UserA',
+recipient: 'UserB',
+tx_id: '995af4d2,3109,407d,bcc0,2fd574267d35'
+}
+],
+prevHash: '000235d64a8a519ae6cc5345061ec1ca87d3f2a5dcfa3c271e6a220f22437286',
+hash: '000be81975b6bff99924f914dc32265fcdf5986426bc7b333cb201265938cce8',
+nonce: 6932
+},
+Block {
+index: 10,
+timestamp: 1717840208,
+transactions: [
+Transaction {
+amount: 42.922185898380214,
+sender: 'UserA',
+recipient: 'UserB',
+tx_id: 'a9a41dda,995c,4c92,9d0d,fa12daa9754d'
+}
+],
+prevHash: '000be81975b6bff99924f914dc32265fcdf5986426bc7b333cb201265938cce8',
+hash: '0000911c501390212f2303d168a594532a51a48d7baa4f1ff139386164c9d0f5',
+nonce: 129214
+},
+Block {
+index: 11,
+timestamp: 1717840209,
+transactions: [
+Transaction {
+amount: 50,
+sender: 'Eve',
+recipient: 'Frank',
+tx_id: '1464c82c,4a05,4485,900a,beec3482a120'
+}
+],
+prevHash: '0000911c501390212f2303d168a594532a51a48d7baa4f1ff139386164c9d0f5',
+hash: '0000a4471c6cb56ff187fc86931c01cc9fbd0af3a2fb44c79618bebf45913ff2',
+nonce: 47040
+}
+],
+pendingTransactions: [],
+difficulty: 4
+}
+Validating blockchain...
+Blockchain is valid: true
